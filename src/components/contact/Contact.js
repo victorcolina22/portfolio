250fc85c128e171423be6ca35359b645090c97b0
Victor Colina <victorcolina2202@gmail.com>
2022-02-26 12:40:20 -0300

    Implementación de traducción para formulario

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -57,27 +57,27 @@ export const Contact = () => {
                 <form
                     id="form"
                     onSubmit={handleSubmit}>
-                    <label htmlFor="name">Name</label>
+                    <label htmlFor="name">{t("form.name")}</label>
                     <input
                         type="text"
                         name="name"
                         onChange={handleInputChange}
                         value={name} />
 
-                    <label htmlFor="email">Email</label>
+                    <label htmlFor="email">{t("form.email")}</label>
                     <input
                         type="email"
                         name="email"
                         onChange={handleInputChange}
                         value={email} />
 
-                    <label htmlFor="message">Message</label>
+                    <label htmlFor="message">{t("form.message")}</label>
                     <textarea
                         name="message"
                         onChange={handleInputChange}
                         value={message} />
 
-                    <button>Enviar</button>
+                    <button>{t("form.send")}</button>
                 </form>
                 {/* <Animator animation={FadeUp}> */}
                 <img className="rocket-img" src={rocket} alt='rocket' />
